1492a8df97681dd0b6f391dd0a7157eeaad1e451
Juan Ignacio Rivero <jr@adhoc.com.ar>
2021-11-29 23:58:14 -0300

    server andando

--- entrega5/entrega_handlebars/app.js
+++ entrega5/entrega_handlebars/app.js
@@ -1,6 +1,11 @@
-import express from 'express';
+import express from "express";
+import * as fs from "fs";
+import {engine} from 'express-handlebars';
+import {Server} from 'socket.io';
+import __dirname from './utils.js';
+
 const app = express();
-const server =  app.listen(8080,()=>{
+const server =  app.listen(5001,()=>{
     console.log("listening");
 })
 
@@ -15,22 +20,32 @@ try {
 }
 
 
-app.set('view engine','ejs');
+
+app.set('views',__dirname+'/views')
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
 app.use(express.static('public'))
 app.use(express.json());
-app.use(express.urlencoded({extender:true}))
-app.get('/',(req,res)=>{
-    let renderObject={
-        nombre:"coders"
-    }
-    res.render('formulario',renderObject)
-})
+// app.use(express.urlencoded({extender:true}))
+
+
+app.get('/', (req, res) => {
+    res.render('formulario');
+});
+
+// app.get('/',(req,res)=>{
+//     let renderObject={
+//         formulario:"info"
+//     }
+//     res.render('formulario',renderObject)
+// })
 
 app.post('/products',(req,res)=>{
     let product={
-        title=req.body.title,
-        price=req.body.price,
-        thumbnail=req.body.thumbnail,
+        title:req.body.title,
+        price:req.body.price,
+        thumbnail:req.body.thumbnail,
 
     }
 products.push(product);
